c4347dc1b30c70de86032a1aa90028ef6ee9eae3
Alexander Konashin <konashin.alexander@yandex.ru>
2023-08-16 20:26:47 +0300

    fix(modal): remove popup close onClick

--- src/pages/modal-page.tsx
+++ src/pages/modal-page.tsx
@@ -16,12 +16,10 @@ const Modal: FC<TModalProps> = ({ children }) => {
     const { state } = useLocation()
     const navigate = useNavigate()
 
-
-    function onDismiss() {
-        navigate(state.backgroundLocation.pathname)
-    }
-
     useEffect(() => {
+        function onDismiss() {
+            navigate(state.backgroundLocation.pathname)
+        }
 
         function closeOnEsc(e: { key: string; }) {
             if (e.key === "Escape" || e.key === "Esc") {
@@ -29,17 +27,29 @@ const Modal: FC<TModalProps> = ({ children }) => {
             }
         }
 
+        function closeOnClick(e: { target: any; }) {
+            const target = e.target
+
+            if (target.localName === 'svg') {
+                onDismiss()
+            } else if (target.localName === 'div' && target.id === 'myOverlay') {
+                onDismiss();
+            }
+        }
+
+        document.addEventListener('click', closeOnClick)
         document.addEventListener("keyup", closeOnEsc);
         return () => {
             document.removeEventListener("keyup", closeOnEsc);
+            document.removeEventListener('click', closeOnClick)
         }
     })
 
     return createPortal(
-        <div className={styles.overlay} onClick={onDismiss}>
+        <div className={styles.overlay} id='myOverlay'>
             <div className={styles.modal} id='myModal'>
                 <div className={styles.modal_box} >
-                    <CloseIcon type={'primary'} onClick={onDismiss} />
+                    <CloseIcon type={'primary'} />
                 </div>
                 {children}
             </div>
